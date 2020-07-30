273bf48b2abd2fd37f57a30bc052a3512a99f3cf
Khushbu Joshi Singhal <khushbujoshisinghal@Khushbus-MacBook-Pro.local>
2020-07-30 00:13:23 -0700

    docs: Update document strings in bikeshare.py

--- script/bikeshare.py
+++ script/bikeshare.py
@@ -9,57 +9,76 @@ MONTHS = ['january', 'february', 'march', 'april', 'may', 'june']
 DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
 def validate_city(city):
+    """Validate user input for city.
+
+    Args:
+        (str) city -> name of the city for which user wants to view data
+
+    Return:
+        (str) city - validated correct city name
     """
-    Checks user input for city.
-    Returns:
-        (str) city - name of the city to analyze
-    """
+
     while city.lower() not in CITY_DATA:
         print('Oops! That is not a valid city name')
         city = input('Would you like to see data for Chicago, New York or Washington:\n')
     return city
+
 def validate_month(month):
+    """Validate user input for month.
+
+    Args:
+        (str) month -> name of the month for which user wants to view data
+
+    Return:
+        (str) month - validated correct month name to filter the data by
     """
-    Checks user input for month.
-    Returns:
-        (str) month - name of the month to filter by
-    """
+
     while month.lower() not in MONTHS:
         print('Oops! That is not a valid month')
         month = input('Please enter for which month: January, February, March, April, May, June\n')
     return month
+
 def validate_day(day):
+    """Validate user input for day.
+
+    Args:
+        (str) day -> name of the day for which user wants to view data
+
+    Return:
+        (str) day - validated correct day of thhe week to filter the data by
     """
-    Checks user input for day.
-    Returns:
-        (str) day - name of the day of week to filter by
-    """
+
     while day.title() not in DAYS:
         print('Oops! That is not a valid day')
         day = input('Please enter for which day: Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday\n')
     return day
+
 def validate_input(timeframe):
+    """Validate user choice for filtering the data by month, day, both or none.
+
+    Args:
+        (str) timeframe -> user choice of timeframe to view data
+
+    Return:
+        (str) month - "month" if user chooses to filter by month
+        (str) day - "day" if user chooses to filter by day of the week
+        (str) both - "both" if user chooses to filter by both month and day of the week
+        (str) none - "none" if user chooses to view unfiltered data
     """
-    Checks userfor filtering the data by month, day, both or none.
-    Returns:
-        (str) month - name of the month to filter by
-        (str) day - name of the day of week to filter by
-        (str) both - name of the month and day of week to filter by
-        (str) none - for unfiltered data
-    """
+
     while timeframe.lower() not in ['month', 'day', 'both', 'none']:
         print('Oops! That is not a valid input')
         timeframe = input('Would you like to filter the data by month, day, both or not at all? Type "none" for no filter\n')
     return timeframe.lower()
 
 def get_filters():
-    """
-    Asks user to specify a city, month, and day to analyze.
+    """Take user input for a city, month, and/or day to filter data.
 
-    Returns:
-        (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+    Return:
+        (str) city - name of the city to filter data
+        (str) timeframe - user's choice of timeframe to filter data
+        (str) month - name of the month to filter data
+        (str) day - name of the day of week to filter data
     """
     city = input('Would you like to see data for Chicago, New York or Washington:\n')
     city = validate_city(city)
@@ -69,6 +88,7 @@ def get_filters():
 
     timeframe = input('\nWould you like to filter the data by month, day, both or not at all? Type "none" for no filter\n')
     timeframe = validate_input(timeframe)
+
     if timeframe == 'month':
         month = validate_month(input('\nPlease enter for which month: January, February, March, April, May, June\n'))
         print('\nShowing {} data for {}'.format(city.title(), month.title()))
@@ -85,14 +105,14 @@ def get_filters():
     return city, month, day, timeframe
 
 def load_data(city,month,day):
-    """
-    Loads data for the specified city and filters by month and/or day if applicable.
+    """Load data for the city of user's choice and for the timeframe specified by the user
 
     Args:
-        (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
-    Returns:
+        (str) city - name of the city to filter data
+        (str) month - name of the month to filter data
+        (str) day - name of the day of week to filter data
+
+    Return:
         df - pandas DataFrame containing city data filtered by month, day, both or neither
     """
     # load data into dataframe
@@ -115,12 +135,13 @@ def load_data(city,month,day):
     return df
 
 def time_stats(df, timeframe):
-    """
-    Calculates time statistics for the filtered or unfiltered data
+    """Calculate time statistics for the filtered or unfiltered data
 
     Args:
         (dataframe) df - pandas DataFrame containing city data filtered by month, day, both or neither
         (str) timeframe - parameter which is used to filter data (month, day, both, none)
+
+    Display descriptive results of the time statistics for the filtered data
     """
     print('\nCalculating time statistics')
 
@@ -154,11 +175,12 @@ def time_stats(df, timeframe):
     print('Execution time:', (time.time() - start_time))
 
 def station_stats(df):
-    """
-    Calculates location statistics for the filtered or unfiltered data
+    """Calculate location statistics for the filtered or unfiltered data
 
     Args:
         (dataframe) df - pandas DataFrame containing city data filtered by month, day, both or neither
+
+    Display descriptive results of the location statistics for the filtered data
     """
     print('\nCalculating location statistics')
 
@@ -182,12 +204,13 @@ def station_stats(df):
     print('Execution time:', (time.time() - start_time))
 
 def user_stats(df,city):
-    """
-    Calculates user statistics for the filtered or unfiltered data
+    """Calculate user statistics for the filtered or unfiltered data
 
     Args:
         (dataframe) df - pandas DataFrame containing city data filtered by month, day, both or neither
         (str) city - city for which statistics are calculated
+
+    Display descriptive results of the user statistics for the filtered data
     """
     print('\nCalculating user statistics')
 
@@ -227,8 +250,7 @@ def user_stats(df,city):
         print('Execution time:', (time.time() - start_time))
 
 def load_raw_data(city,counter):
-    """
-    Loads raw data as is 5 rows at a time
+    """Load raw data as is 5 rows at a time
 
     Args:
         (int) counter - counter
@@ -242,35 +264,28 @@ def load_raw_data(city,counter):
         exit()
 
 def valid_input(raw_data):
-    """
-    Validates user input.
-    Returns:
-        (str) raw_data - yes: if user wants to see raw dat
+    """Validate user input.
+
+    Return:
+        (str) raw_data - yes: if user wants to see raw data
                          no: if user does not want to see raw data
     """
+
     while raw_data not in ('yes', 'no'):
         print('\nOops! That is not a valid input')
         raw_data = input('Would you like to see raw data? Please enter "yes" or "no"\n')
     return raw_data
-
-#raw_data = input('\nWould you like to see raw data? Type "yes" or "no"\n')
-#raw_data = valid_input(raw_data)
-#counter = 0
-#while raw_data == 'yes':
-#    load_raw_data(city, counter)
-#    counter += 5
-#    raw_data = input('\nWould you like to see next 5 rows of raw data? yes or no\n')
-#    raw_data = valid_input(raw_data)
-
 
 def main():
     while True:
         city, month, day, timeframe = get_filters()
         df = load_data(city, month, day)
 
+        #Function call for time statistics of filtered data
         time_stats(df, timeframe)
+        #Function call for location statistics of filtered data
         station_stats(df)
-        #trip_duration_stats(df)
+        #Function call for user statistics of filtered data
         user_stats(df, city)
         raw_data = valid_input(input('\nWould you like to see raw data? Type "yes" or "no"\n'))
         counter = 0
